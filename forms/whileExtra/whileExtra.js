c70df252061fd17f6ce813c3c2920c8741345c53
Lizzie <ekr74869@creighton.edu>
2019-09-25 16:01:53 -0500

    Update whileExtra.js

--- forms/whileExtra/whileExtra.js
+++ forms/whileExtra/whileExtra.js
@@ -1,1 +1,7 @@
-let name = prompt("What is your first and last name?")
+let fullName = prompt("What is your first and last name?")
+i = 0
+while(i < fullName.length) {
+  console.log(fullName.charAt(i))
+  i++;
+  }
+  
